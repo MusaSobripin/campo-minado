a3357b60ff08e68a432de11fc580ee4aa835879e
MusaSobripin <moisespinhao2001@gmail.com>
2023-10-18 13:40:38 -0300

    criação de testes

--- interacao_botoes_test.py
+++ interacao_botoes_test.py
@@ -1,7 +1,6 @@
 import pytest
 from tkinter import Tk
-# from pytest_tkinter import TkFixture
-from cminado import CampoMinado  # Substitua 'seu_modulo' pelo nome real do seu módulo
+from cminado import CampoMinado  
 
 @pytest.fixture
 def campo_minado():
@@ -11,18 +10,101 @@ def campo_minado():
     yield game
     root.quit()
 
-# def test_interacao_de_botoes(campo_minado):
-#     campo_minado.nivel = "Fácil"
+def test_interacao_de_botoes_facil(campo_minado):
+    # Defina o nível desejado
+    campo_minado.iniciar_jogo (8, 8, 10)
+    campo_minado.nivel_desejado = "Fácil"
+    
+    def pressiona_botao():
+        campo_minado.botao_texto = 'Fácil'  # Use o nível desejado
+        botao_encontrado = None
+    
+        # Iterar pelos botões em busca do botão com o texto desejado
+        for widget in campo_minado.root.winfo_children():
+            if widget.cget("text") == campo_minado.botao_texto:
+                botao_encontrado = widget
+                break
+    
+        if botao_encontrado:
+            botao_encontrado.invoke()
+        else:
+            raise AssertionError(f"Botão com texto '{campo_minado.botao_texto}' não encontrado na interface.")
+    
+    campo_minado.label, campo_minado.buttons = campo_minado.criar_menu_niveis()
+    campo_minado.root.update_idletasks()  # Garante que a interface esteja atualizada
+    
+    campo_minado.root.after(2000, pressiona_botao)
+    
+    # Verifique se a função iniciar_jogo foi chamada com os parâmetros corretos
+    nivel_esperado = (8, 8, 10)
+    resultado = campo_minado.nivel
+    
+    # Verifique se o nível selecionado corresponde ao esperado
+    assert resultado == nivel_esperado, f"O nível selecionado deveria ser '{nivel_esperado}', mas foi '{resultado}'."
 
-#     def pressiona_botao():
-#         botao = campo_minado.root.nametowidget(".!button")  # Suponha que o botão seja o único na janela
-#         botao.invoke()
+def test_interacao_de_botoes_intermediario(campo_minado):
+    # Defina o nível desejado
+    campo_minado.iniciar_jogo (10, 16, 30)
+    campo_minado.nivel_desejado = "Intermediário"
+    
+    def pressiona_botao():
+        campo_minado.botao_texto = 'Intermediário'  # Use o nível desejado
+        botao_encontrado = None
+    
+        # Iterar pelos botões em busca do botão com o texto desejado
+        for widget in campo_minado.root.winfo_children():
+            if widget.cget("text") == campo_minado.botao_texto:
+                botao_encontrado = widget
+                break
+    
+        if botao_encontrado:
+            botao_encontrado.invoke()
+        else:
+            raise AssertionError(f"Botão com texto '{campo_minado.botao_texto}' não encontrado na interface.")
+    
+    campo_minado.label, campo_minado.buttons = campo_minado.criar_menu_niveis()
+    campo_minado.root.update_idletasks()  # Garante que a interface esteja atualizada
+    
+    campo_minado.root.after(2000, pressiona_botao)
+    
+    # Verifique se a função iniciar_jogo foi chamada com os parâmetros corretos
+    nivel_esperado = (10, 16, 30)
+    resultado = campo_minado.nivel
+    
+    # Verifique se o nível selecionado corresponde ao esperado
+    assert resultado == nivel_esperado, f"O nível selecionado deveria ser '{nivel_esperado}', mas foi '{resultado}'."
 
-#     campo_minado.root.after(2000, pressiona_botao)
-
-#     resultado = campo_minado.iniciar_jogo(8, 8, 10)
-
-#     assert resultado == (8, 8, 10), f"A função self.iniciar_jogo deveria ser chamada com os parâmetros corretos, mas recebeu {resultado}."
+def test_interacao_de_botoes_dificil(campo_minado):
+    # Defina o nível desejado
+    campo_minado.iniciar_jogo (24, 24, 100)
+    campo_minado.nivel_desejado = "Difícil"
+    
+    def pressiona_botao():
+        campo_minado.botao_texto = 'Difícil'  # Use o nível desejado
+        botao_encontrado = None
+    
+        # Iterar pelos botões em busca do botão com o texto desejado
+        for widget in campo_minado.root.winfo_children():
+            if widget.cget("text") == campo_minado.botao_texto:
+                botao_encontrado = widget
+                break
+    
+        if botao_encontrado:
+            botao_encontrado.invoke()
+        else:
+            raise AssertionError(f"Botão com texto '{campo_minado.botao_texto}' não encontrado na interface.")
+    
+    campo_minado.label, campo_minado.buttons = campo_minado.criar_menu_niveis()
+    campo_minado.root.update_idletasks()  # Garante que a interface esteja atualizada
+    
+    campo_minado.root.after(2000, pressiona_botao)
+    
+    # Verifique se a função iniciar_jogo foi chamada com os parâmetros corretos
+    nivel_esperado = (24, 24, 100)
+    resultado = campo_minado.nivel
+    
+    # Verifique se o nível selecionado corresponde ao esperado
+    assert resultado == nivel_esperado, f"O nível selecionado deveria ser '{nivel_esperado}', mas foi '{resultado}'."
 
 def test_numero_de_botoes(campo_minado):
     
@@ -42,9 +124,8 @@ def test_ordem_dos_botoes(campo_minado):
 
     campo_minado.root.destroy()
     
-# def test_manipulacao_de_eventos(campo_minado):
-#     # Simule um clique no botão de nível "Fácil"
-#     campo_minado.nivel = "Fácil"
+# def test_manipulacao_de_eventos_facil(campo_minado):
+#     campo_minado.iniciar_jogo(8, 8, 10)
 
 #     # Obtenha todos os botões no frame do menu
 #     botoes = campo_minado.root.winfo_children()
@@ -63,6 +144,9 @@ def test_ordem_dos_botoes(campo_minado):
     
 #     campo_minado.root.after(2000, pressiona_botao)
 
-#     resultado = campo_minado.iniciar_jogo(8, 8, 10)
+#     # Espere até que o jogo seja iniciado
+#     campo_minado.root.wait_variable(campo_minado.root.winfo_children()[0].iniciar)
 
-#     assert resultado == (8, 8, 10), f"A função self.iniciar_jogo deveria ser chamada com os parâmetros corretos, mas recebeu {resultado}."
+#     # Verifique se o jogo foi iniciado com os parâmetros corretos
+#     resultado = campo_minado.iniciar_jogo(8, 8, 10)
+#     assert resultado == "Fácil", f"O nível selecionado deveria ser 'Fácil', mas foi '{resultado}'."
