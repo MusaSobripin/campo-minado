6e13427cf3e6b16ab8ae5b3fe504a7876bd1c540
MusaSobripin <moisespinhao2001@gmail.com>
2023-11-18 21:54:47 -0300

    organização de arquivo

---
+++ tests/desempenho_tabuleiro_test.py
@@ -0,0 +1,71 @@
+import pytest
+import time
+import timeit
+from tkinter import Tk
+from cminado import CampoMinado 
+
+@pytest.fixture
+def campo_minado():
+    root = Tk()
+    root.withdraw()
+    game = CampoMinado(root)
+    yield game
+    root.quit()
+    
+def test_desempenho_tabuleiro_pequeno_facil(campo_minado):
+    campo_minado.iniciar_jogo(8, 8, 10)
+    x, y = 3, 3
+    tempo_inicio = time.time()
+    campo_minado.calcular_vizinhos(x, y)
+    tempo_fim = time.time()
+    tempo_execucao = tempo_fim - tempo_inicio
+    assert tempo_execucao < 1, "Tempo de execução para tabuleiro pequeno é maior que 1 segundo."
+
+def test_desempenho_tabuleiro_pequeno_intermediario(campo_minado):
+    campo_minado.iniciar_jogo(10, 16, 30)
+    x, y = 3, 3
+    tempo_inicio = time.time()
+    campo_minado.calcular_vizinhos(x, y)
+    tempo_fim = time.time()
+    tempo_execucao = tempo_fim - tempo_inicio
+    assert tempo_execucao < 1, "Tempo de execução para tabuleiro pequeno é maior que 1 segundo."
+
+def test_desempenho_tabuleiro_pequeno_dificil(campo_minado):
+    campo_minado.iniciar_jogo(24, 24, 100)
+    x, y = 3, 3
+    tempo_inicio = time.time()
+    campo_minado.calcular_vizinhos(x, y)
+    tempo_fim = time.time()
+    tempo_execucao = tempo_fim - tempo_inicio
+    assert tempo_execucao < 1, "Tempo de execução para tabuleiro pequeno é maior que 1 segundo."
+
+
+def test_desempenho_time_tabuleiro_pequeno(campo_minado):
+    campo_minado.iniciar_jogo(8, 8, 10)
+    x, y = 3, 3
+
+    def calcular_vizinhos():
+        campo_minado.calcular_vizinhos(x, y)
+
+    tempo_execucao = timeit.timeit(calcular_vizinhos, number=1000)  # Número de execuções para obter uma medição precisa
+    assert tempo_execucao < 1, "Tempo de execução para tabuleiro pequeno é maior que 1 segundo."
+
+
+def test_desempenho_tabuleiro_medio(campo_minado):
+    campo_minado.iniciar_jogo(10, 16, 30)
+    x, y = 8, 8
+    tempo_inicio = time.time()
+    campo_minado.calcular_vizinhos(x, y)
+    tempo_fim = time.time()
+    tempo_execucao = tempo_fim - tempo_inicio
+    assert tempo_execucao < 1, "Tempo de execução para tabuleiro médio é maior que 1 segundo."
+
+def test_desempenho_tabuleiro_grande(campo_minado):
+    campo_minado.iniciar_jogo(24, 24, 100)
+    x, y = 16, 16
+    tempo_inicio = time.time()
+    campo_minado.calcular_vizinhos(x, y)
+    tempo_fim = time.time()
+    tempo_execucao = tempo_fim - tempo_inicio
+    assert tempo_execucao < 1, "Tempo de execução para tabuleiro grande é maior que 1 segundo."
+
